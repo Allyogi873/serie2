d1518d2760878b8300d3ff7742e6dc5a1955b922
Allyogi873 <allan_quezada@hotmail.com>
2021-03-16 21:00:40 -0600

    commit

---
+++ src/analisis_parcial2/persona.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analisis_parcial2;
+
+/**
+ *
+ * @author allan
+ */
+public abstract class persona {
+    public abstract String nombreCompleto();
+}
